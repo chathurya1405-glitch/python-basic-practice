1aae6cb856e2c89a564a86a86cdad89e8366e8e8
chathurya1405-glitch <chathurya1405@gmail.com>
2026-07-30 18:24:02 +0530

    Create hello2.py

---
+++ hello2.py
@@ -0,0 +1,68 @@
+s="Python"
+print(s,type(s))
+
+s='CHATHURYA'
+print(s,type(s))
+
+age=19
+print(type(age))
+print("My age is" + str(age))
+
+s="""PALLAVI"""
+print(s,type(s))
+
+addr1 = """Chathurya Pallavi
+HNO: 5-2-119
+Hyderabad
+Telangana 500070"""
+
+print(addr1, type(addr1))
+
+addr2 = '''Rahul Sharma
+FNO: 2-6-45
+Secunderabad
+Telangana 500070'''
+
+print(addr2, type(addr2))
+
+s = "CHATHURYA"
+
+print(s[3])
+print(s[5])
+print(s[-2])
+print(s[1])
+
+s="SRIINDU"
+print(s[1:6])
+print(s[2:6])
+print(s[-3:-4])
+print(s[0:6])
+
+s="MUKESH"
+print(s[1:])
+print(s[-2:])
+print(s[0:])
+print(s[4:])
+
+s="PLAYING"
+print(s[:])
+
+s="hari"
+print(s[:3])
+print(s[:0])
+print(s[:-2])
+
+s = "MEDISETTI"
+print(s[:5])
+print(s[:2])
+print(s[:-4])
+
+s = "INTERNSHIP"
+print(s[8:1:-1])
+print(s[7:2:-1])
+print(s[9:2:-2])
+print(s[-2:-6:-1])
+print(s[::-3])
+print(s[::-4])
+print(s[:0:1])
+print(s[:-1:-1])
